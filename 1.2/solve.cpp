eb96c23b077cd3dfdb2b1a0865f3cd6a8a73661e
Diego Assencio <diego@assencio.com>
2015-09-27 19:52:15 +0200

    adds solution to problem 1.2

---
+++ 1.2/solve.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <cstring>	/* std::strlen */
+#include <algorithm>	/* std::reverse */
+#include <cassert>
+
+/**
+ * @brief reverses an input C-string in-place
+ * @param pointer to a C-string (null-terminated)
+ * @return pointer to the (reversed) input string
+ * @note complexity: O(n) in time, O(1) in space
+ */
+char* reverse_c_string(char* str)
+{
+	size_t len = std::strlen(str);
+
+	if (len <= 1)
+	{
+		return str;
+	}
+
+	char* first = str;
+	char* last = str + len - 1;
+
+	while (first < last)
+	{
+		std::swap(*first, *last);
+		first++;
+		last--;
+	}
+
+	return str;
+}
+
+
+/**
+ * @brief generates a random C-string of length n and characters in [a-z]
+ * @note complexity: O(n) in both space and time
+ */
+char* random_string(size_t n)
+{
+	std::random_device device;
+	std::mt19937 generator(device());
+	std::uniform_int_distribution< int > distribution(0,25);
+
+	char* str = new char[n+1];
+	str[n] = '\0';
+
+	while (n > 0)
+	{
+		--n;
+		str[n] = 'a' + distribution(generator);
+	}
+
+	return str;
+}
+
+int main()
+{
+	for (int n = 0; n <= 100; ++n)
+	{
+		for (int i = 0; i < 1000; ++i)
+		{
+			char* str = random_string(n);
+			std::string str_copy(str);
+
+			reverse_c_string(str);
+			std::reverse(str_copy.begin(), str_copy.end());
+
+			assert(strcmp(str, str_copy.c_str()) == 0);
+
+			delete[] str;
+		}
+
+		std::cout << "passed random tests for strings of length " << n << std::endl;
+	}
+
+	return 0;
+}
