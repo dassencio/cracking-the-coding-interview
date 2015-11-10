fb5fdc9ff4ed5c1b62be7c3649ecbba1800fa08e
Diego Assencio <diego@assencio.com>
2015-11-10 12:32:23 +0100

    removes templates from problem 1.6

--- 1.6/solve.cpp
+++ 1.6/solve.cpp
@@ -5,7 +5,6 @@
 #include <cassert>
 
 /* a simple matrix class */
-template< typename T >
 class square_matrix
 {
 public:
@@ -14,13 +13,13 @@ public:
 		/* nothing needs to be done here */
 	}
 
-	T& operator()(const size_t i, const size_t j)
+	int& operator()(const size_t i, const size_t j)
 	{
 		assert(i < n_ && j < n_);
 		return values_[n_*i + j];
 	}
 
-	const T& operator()(const size_t i, const size_t j) const
+	int operator()(const size_t i, const size_t j) const
 	{
 		assert(i < n_ && j < n_);
 		return values_[n_*i + j];
@@ -60,12 +59,11 @@ private:
  * @return copy of input matrix rotated 90 degrees counterclockwise
  * @note complexity: O(n²) in time, O(n²) in space
  */
-template< typename T >
-square_matrix< T > rotate_square_matrix_1(const square_matrix< T >& A)
+square_matrix rotate_square_matrix_1(const square_matrix& A)
 {
 	size_t n = A.size();
 
-	square_matrix< T > A_rot(n);
+	square_matrix A_rot(n);
 
 	for (size_t i = 0; i < n; ++i)
 	{
@@ -84,8 +82,7 @@ square_matrix< T > rotate_square_matrix_1(const square_matrix< T >& A)
  * @return reference to input matrix (rotatetd 90 degrees counterclockwise)
  * @note complexity: O(n²) in time, O(1) in space
  */
-template< typename T >
-square_matrix< T >& rotate_square_matrix_2(square_matrix< T >& A)
+square_matrix& rotate_square_matrix_2(square_matrix& A)
 {
 	size_t n = A.size();
 
@@ -94,15 +91,15 @@ square_matrix< T >& rotate_square_matrix_2(square_matrix< T >& A)
 		for (size_t j = i; j < n-1-i; ++j)
 		{
 			/* (i,j) --> (n-1-j,i) */
-			T a1 = A(n-1-j,i);
+			int a1 = A(n-1-j,i);
 			A(n-1-j,i) = A(i,j);
 
 			/* (n-1-j,i) --> (n-1-i,n-1-j) */
-			T a2 = A(n-1-i,n-1-j);
+			int a2 = A(n-1-i,n-1-j);
 			A(n-1-i,n-1-j) = a1;
 
 			/* (n-1-i,n-1-j) --> (j,n-1-i) */
-			T a3 = A(j,n-1-i);
+			int a3 = A(j,n-1-i);
 			A(j,n-1-i) = a2;
 
 			/* (j,n-1-i) --> (i,j) */
@@ -117,13 +114,13 @@ square_matrix< T >& rotate_square_matrix_2(square_matrix< T >& A)
  * @brief generates a random n×n matrix
  * @note complexity: O(n²) in both space and time
  */
-square_matrix< int > random_matrix(const size_t n)
+square_matrix random_matrix(const size_t n)
 {
 	std::random_device device;
 	std::mt19937 generator(device());
 	std::uniform_int_distribution< int > distribution(0,1000);
 
-	square_matrix< int > A(n);
+	square_matrix A(n);
 
 	for (size_t i = 0; i < n; ++i)
 	{
@@ -142,9 +139,9 @@ int main()
 	{
 		for (int i = 0; i < 1000; ++i)
 		{
-			square_matrix< int > A = random_matrix(n);
+			square_matrix A = random_matrix(n);
 
-			square_matrix< int > A_rot = rotate_square_matrix_1(A);
+			square_matrix A_rot = rotate_square_matrix_1(A);
 			rotate_square_matrix_2(A);
 
 			assert(A == A_rot);
