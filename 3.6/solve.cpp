4f4b684dea4cd38beb246948dcd6feb218702e74
Diego Assencio <diego@assencio.com>
2016-03-05 19:21:11 +0100

    adds solution to problem 3.6

---
+++ 3.6/solve.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <stack>
+#include <random>	/* std::uniform_int_distribution */
+#include <cassert>
+
+/**
+ * @brief sorts a stack A with n elements in ascending order (i.e., the stack
+ *        top will be its minimum element after sorting)
+ * @note complexity: O(n²) in time, O(n) in space
+ */
+template< typename T >
+void sort_stack(std::stack< T >& A)
+{
+	std::stack< T > B;
+
+	while (A.empty() == false)
+	{
+		/* take A's top element, call it a */
+		T a = A.top();
+		A.pop();
+
+		/*
+		 * as long as B's top element is smaller than a, put B's top
+		 * element back on top of A
+		 */
+		while (B.empty() == false && B.top() < a)
+		{
+			A.push(B.top());
+			B.pop();
+		}
+
+		/*
+		 * at this point, B is either empty or has a top element
+		 * which is larger than or equal to a, so put a on B
+		 */
+		 B.push(a);
+	}
+
+	A = B;
+}
+
+int main()
+{
+	std::random_device device;
+	std::mt19937 generator(device());
+	std::uniform_int_distribution< int > distribution;
+
+	for (size_t n = 0; n <= 50; ++n)
+	{
+		for (int i = 0; i < 1000; ++i)
+		{
+			std::stack< int > A;
+
+			/* add elements to the stack */
+			while (A.size() < n)
+			{
+				A.push(distribution(generator));
+			}
+
+			sort_stack(A);
+
+			assert(A.size() == n);
+
+			int a = std::numeric_limits< int >::min();
+
+			/* check if the stack is now sorted */
+			while (A.empty() == false)
+			{
+				assert(A.top() >= a);
+				a = A.top();
+				A.pop();
+			}
+		}
+
+		std::cout << "passed random tests for stacks of size " << n << std::endl;
+	}
+
+	return 0;
+}
