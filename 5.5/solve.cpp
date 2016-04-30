7af74442578df6e2e7a54f385fc02e69cd780394
Diego Assencio <diego@assencio.com>
2016-04-30 14:41:56 +0200

    adds solution to problem 5.5

---
+++ 5.5/solve.cpp
@@ -0,0 +1,66 @@
+/*
+ * TASK: You are given two integer values x and y. Determine the number of
+ *       bits which differ on the binary representations of x and y. As an
+ *       example, if the binary representations of x and y are 10001 and
+ *       10010 respectively, the number of different bits is 2.
+ */
+
+#include <bitset>
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+
+/**
+ * @brief returns the number of bits in x which differ from y
+ * @note complexity: O(1) in both time and space
+ */
+size_t num_different_bits_1(const uint32_t x, const uint32_t y)
+{
+	/*
+	 * each bit of z is set only if it the equivalent bits of x and y
+	 * are different, so the number of bits set to 1 in z is the answer
+	 * we want
+	 */
+	uint32_t z = x ^ y;
+
+	size_t count = 0;
+
+	while (z > 0)
+	{
+		count += z % 2;
+		z >>= 1;
+	}
+
+	return count;
+}
+
+/**
+ * @brief returns the number of bits in x which differ from y using a bitset
+ * @note complexity: O(1) in both time and space (but this solution is better
+ *       than the other one as it can use specialized machine instructions for
+ *       counting the number of bits set on an integer value)
+ */
+size_t num_different_bits_2(const uint32_t x, const uint32_t y)
+{
+	return std::bitset< 32 >(x ^ y).count();
+}
+
+int main()
+{
+	std::random_device device;
+	std::mt19937 generator(device());
+
+	std::uniform_int_distribution< uint32_t > distribution;
+
+	for (uint32_t i = 0; i < 1000000; ++i)
+	{
+		uint32_t x = distribution(generator);
+		uint32_t y = distribution(generator);
+
+		assert(num_different_bits_1(x,y) == num_different_bits_2(x,y));
+	}
+
+	std::cout << "passed random tests" << std::endl;
+
+	return 0;
+}
